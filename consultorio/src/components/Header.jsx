785af8ec87a7a121c5a709ee07953d6c89e08584
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-02-01 14:38:35 -0300

    componente header

---
+++ consultorio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css';
+import { User } from "phosphor-react";
+
+export function Header(){
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.headerInfo}>
+          <p>ELISABETH APARECIDA</p>
+          <p>Nazaré Paulista - SP</p>
+        </div>
+        <User size={40}/>
+      </div>
+    </header>
+  );
+}

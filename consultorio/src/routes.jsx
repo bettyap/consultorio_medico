85dc1ace932367f33f4e3c9a09258097612fe147
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-02-02 23:23:50 -0300

    feat: create route

--- consultorio/src/routes.jsx
+++ consultorio/src/routes.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Header } from './components/Header';
 import Dashboard from './pages/Dashboard/Dashboard';
+import { Scheduling } from './pages/Scheduling/Scheduling';
 
 export default function Router(){
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/scheduling" element={<Scheduling/>} />
       </Routes>
     </BrowserRouter>
   );

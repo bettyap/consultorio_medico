3ced99c26c7fc5604f66cfe98c955646747f3cfa
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-02-05 18:26:27 -0300

    feat: add responsive

--- consultorio/src/pages/Scheduling/Scheduling.jsx
+++ consultorio/src/pages/Scheduling/Scheduling.jsx
@@ -6,13 +6,50 @@ import styles from "./Scheduling.module.css";
 import { ListScheduling } from "../../components/ListScheduling";
 import { useAuth } from "../../providers/auth";
 import { useScheduling } from "../../providers/scheduling";
+import { useState } from "react";
+import dayjs from 'dayjs';
+
+const hoursScheduling=[
+  "08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30",
+  "13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30"
+]
 
 export function Scheduling(){
+  const [date, setDate] = useState()
+
   const { doctors  } = useAuth()
   const { schedulings, setSchedulings } = useScheduling()
+  const [ schedulingCurrent, setSechedulingCurrent ] = useState([])
+  // const todaySchedulings = []
 
-  console.log(schedulings)
   
+  function setActiveDate (value) {
+    setDate(value)
+    let agendamentosDoDia = schedulings.filter((scheduling, index) => {
+      const scheduleDate = dayjs(scheduling.datetime).format("DD/MM/YYYY")
+      const activeDate = dayjs(value).format("DD/MM/YYYY")
+
+      if( scheduleDate === activeDate) {
+        return true
+      } else {
+        return false
+      }
+    })
+
+    
+    // cria o array de horas e mergeia com os agendamentos do dia
+    for(let i = 0; i < hoursScheduling.length; i++ ){
+      const hour = hoursScheduling[i] 
+      let agendamento = agendamentosDoDia.find(agendamento => {
+        return agendamento === hour
+      })
+
+    }
+    setSechedulingCurrent(agendamentosDoDia)
+
+    console.log(agendamentosDoDia)
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -31,17 +68,17 @@ export function Scheduling(){
               </div>
             </div>
             <div className={styles.calendarContainer}>
-              <CalendarSimple />
+              <CalendarSimple onChange={setActiveDate} value={date} />
             </div>
           </section>
           <section className={styles.main2}>
-            Horas
+            {dayjs(date).format("DD/MMMM YYYY")}
             <div className={styles.contentScheduling}>
               {
-                schedulings.map((scheduling) => (
+                schedulingCurrent.map((scheduling) => (
                   <ListScheduling
                     key={scheduling.patientCpf} 
-                    hours={scheduling.datetime}
+                    hours={dayjs(scheduling.datetime).format("HH:mm")}
                     name={scheduling.patientName}
                   />
                 ))

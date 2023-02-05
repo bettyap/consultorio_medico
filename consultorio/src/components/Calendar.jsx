d92c0f6886bcc3b1bb0afd5ee94d3c6556833df9
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-02-05 18:32:12 -0300

    fix: add at the component Calendar props

--- consultorio/src/components/Calendar.jsx
+++ consultorio/src/components/Calendar.jsx
@@ -2,10 +2,10 @@ import './Calendar.css'
 import styles from "./Calendar.module.css";
 import { Calendar } from 'react-calendar';
 
-export function CalendarSimple(){
+export function CalendarSimple(props){
   return(
     <div className={styles.calendarContainer}>
-      <Calendar />
+      <Calendar {...props}/>
     </div>
   );
 }

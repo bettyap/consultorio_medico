02391873d6a4dee97bb655f6d440d496394c59cc
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-02-05 18:34:18 -0300

    feat: add route page consult

--- consultorio/src/routes.jsx
+++ consultorio/src/routes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header';
+import { Consult } from './pages/Consult/Consult';
 import Dashboard from './pages/Dashboard/Dashboard';
 import { Scheduling } from './pages/Scheduling/Scheduling';
 
@@ -10,6 +11,7 @@ export default function Router(){
       <Routes>
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/scheduling" element={<Scheduling/>} />
+        <Route path="/consult" element={<Consult/>} />
       </Routes>
     </BrowserRouter>
   );

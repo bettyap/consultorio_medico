51c0248825c1bb97128de840a43cdcd05e66f504
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-02-07 09:03:56 -0300

    fix: classname improvements List Scheduling

--- consultorio/src/components/ListScheduling.jsx
+++ consultorio/src/components/ListScheduling.jsx
@@ -4,18 +4,17 @@ import styles from "./ListScheduling.module.css";
 export function ListScheduling(props){
 
   return(
-    <div className={styles.contentDoctorList}>
+    <div className={styles.containerListScheduling}>
       <span>{props.hours}</span>
-      <div className={styles.listInfo}>
+      <div className={styles.contentListScheduling}>
         <UserCircle size={48} color="var(--green-500)" />
         <p>{props.name}</p>
       </div>
-      <div className={styles.crud}>
+      <div className={styles.listSchedulingIcon}>
         {
           props.name === undefined 
           ?<PlusCircle size={28} color="var(--green-500)" onClick={props.onAdd} />       
           :<Trash size={28} color="var(--green-500)" onClick={props.onDelete}/>
-
         }
       </div>
     </div>

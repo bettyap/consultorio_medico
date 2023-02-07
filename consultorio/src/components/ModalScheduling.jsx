13d4e2538f80d8cc3302694267295f32f4f32b75
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-02-07 09:04:36 -0300

    fix: classname improvements modal

--- consultorio/src/components/ModalScheduling.jsx
+++ consultorio/src/components/ModalScheduling.jsx
@@ -18,34 +18,38 @@ export function ModalScheduling(props){
   }
 
   return(
-    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+    <form className={styles.containerModal} onSubmit={handleSubmit(onSubmit)}>
 
-      <label htmlFor="cpf">CPF</label>
+      <label className={styles.modalLabel} htmlFor="cpf">CPF</label>
       <InputMask 
+        className={styles.modalInput}
         mask="999.999.999-99"
         placeholder="999.999.999-99"
         {...register('cpf',{required: true })}
       />
       {errors.cpf && <span className={styles.formError}>Esse campo é obrigatório</span>}
 
-      <label htmlFor="birthDate">Data de nascimento: </label>
-      <InputMask 
+      <label className={styles.modalLabel} htmlFor="birthDate">Data de nascimento: </label>
+      <InputMask
+        className={styles.modalInput} 
         mask="99/99/9999"
         placeholder="01/01/1972"
         {...register('birthDate',{required: true })}
       />
       {errors.birthDate && <span className={styles.formError}>Esse campo é obrigatório</span>}
 
-      <label htmlFor="patientName">Nome completo:</label>
-      <InputMask 
+      <label className={styles.modalLabel} htmlFor="patientName">Nome completo:</label>
+      <InputMask
+        className={styles.modalInput} 
         mask=""
         placeholder="Maria da Silva"
         {...register('patientName',{required: true })}
       />
       {errors.patientName && <span className={styles.formError}>Esse campo é obrigatório</span>}
 
-      <label htmlFor="phoneNumber">Telefone:</label>
-      <InputMask 
+      <label className={styles.modalLabel} htmlFor="phoneNumber">Telefone:</label>
+      <InputMask
+        className={styles.modalInput} 
         mask="(99)99999-9999"
         placeholder="(11)99999-9999"
         {...register('phoneNumber',{required: true })}
@@ -53,31 +57,33 @@ export function ModalScheduling(props){
       {errors.phoneNumber && <span className={styles.formError}>Esse campo é obrigatório</span>}
 
 
-      <label htmlFor="value">Valor</label>
+      <label className={styles.modalLabel} htmlFor="value">Valor</label>
       <CurrencyInput
+        className={styles.modalInput}
         placeholder="R$ 100,00" 
         type="number"
         {...register('value',{required: true })}
       />
       {errors.value && <span className={styles.formError}>Esse campo é obrigatório</span>}
 
-      <label htmlFor="doctorCrm">Valor</label>
-      <select {...register('doctorCrm', {required: true})}>
+      <label className={styles.modalLabel} htmlFor="doctorCrm">Valor</label>
+      <select className={styles.modalSelect} {...register('doctorCrm', {required: true})}>
         {doctors.map(doctor => (
           <option key={doctor.crm} value={doctor.crm}>{doctor.name}</option>
         ))}
       </select>
       {errors.doctorCrm && <span className={styles.formError}>Esse campo é obrigatório</span>}
       
-      <label htmlFor="paymentForm">Forma de pagamento:</label>
-      <InputMask 
+      <label className={styles.modalLabel} htmlFor="paymentForm">Forma de pagamento:</label>
+      <InputMask
+        className={styles.modalInput} 
         mask=""
         placeholder="Ex. Dinheiro"
         {...register('paymentForm',{required: true })}
       />
       {errors.paymentForm && <span className={styles.formError}>Esse campo é obrigatório</span>}
 
-      <button type="submit" className={styles.btnModal}>
+      <button type="submit" className={styles.modalBtn}>
         Confirmar
       </button>
     </form>
